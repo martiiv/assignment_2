f5c8efef28c653f68854dfefcba9da56fa5c91a5
Martin Iversen <martiiv@ntnu.no>
2021-03-13 16:06:00 +0100

    Updated getCountryInfo method will create new method(using 2nd endpoint) for getting info within timeframe in following commits

--- country.go
+++ country.go
@@ -7,6 +7,24 @@ import (
 	"net/http"
 )
 
+type CountryInfo struct {
+	Country             string
+	Population          int
+	Sq_km_area          int
+	Life_expectancy     string
+	Elevation_in_meters int
+	Continent           string
+	Abbreviation        string
+	Location            string
+	Iso                 int
+	Capital_city        string
+	Dates               []string
+}
+
+type Date struct {
+	Dates string
+}
+
 /*
  * This file contains the country endpoint
  * This endpoint is used for getting the corona cases of a given country
@@ -18,12 +36,12 @@ import (
 //TODO Implement endpoint
 //TODO Implement date
 //TODO Handle errors
+//TODO implement other endpoiint for date functionality
 func getCountryInfo(w http.ResponseWriter, r *http.Request) {
 	client := &http.Client{}
 	vars := mux.Vars(r)
 	country := vars["country_name"]
-	//date := vars["?begin_date-end_date"]
-	url := "https://covid-api.mmediagroup.fr/v1/cases?country=" + country + ""
+	url := "https://covid-api.mmediagroup.fr/v1/history?country=" + country + "&status=Confirmed"
 	fmt.Print(url)
 	r, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
@@ -37,14 +55,17 @@ func getCountryInfo(w http.ResponseWriter, r *http.Request) {
 	}
 
 	//Fetches json from the request
-	output, err := ioutil.ReadAll(res.Body)
+	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		http.Error(w, "Error when parsing json response: ", http.StatusGone)
 	}
 
-	fmt.Fprint(w, string(output))
+	fmt.Fprint(w, string(body))
 	if err != nil {
 		http.Error(w, "Error ocurred when displaying content", http.StatusInternalServerError)
 	}
+}
+
+func getDatesInCountry(w http.ResponseWriter, r *http.Request) {
 
 }
