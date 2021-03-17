3a88be1b6a5882111534277368e614bf5bc4973b
Martin Iversen <Martin>
2021-03-17 15:28:24 +0100

    Updated endpoint 2 with function getPolicy() and added in formatOutput function

--- policy.go
+++ policy.go
@@ -7,6 +7,19 @@ import (
 	"net/http"
 )
 
+type PolicyActions struct {
+	PolicyActions StringencyData
+}
+
+type StringencyData struct {
+	Date_value        string
+	Country_code      string
+	Confirmed         int
+	Deaths            int
+	Stringency_actual float64
+	Stringency        float64
+}
+
 /**
  * Struct called Currency used for parsing JSON
  * Structure derived from: https://exchangeratesapi.io/
@@ -21,6 +34,21 @@ type Currency struct {
 	Symbol string
 }
 
+func formatOutput(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	countryName := vars["country_name"]
+	startDate := vars["begin_date"] //Start date from url
+	endDate := vars["end_date"]     //End date from url
+	policy := getPolicy(w, r)
+
+	//Formatting output as specified in assignment
+	fmt.Fprintf(w, "country:"+countryName+"\n")
+	fmt.Fprintf(w, "scope: "+startDate+"-"+endDate+"\n")
+	fmt.Fprintf(w, "stringency:%v \n", policy.PolicyActions.Stringency)
+	fmt.Fprintf(w, "trend:%v\n")
+}
+
 /*
  * This file contains code for the policy endpoint
  * This endpoint will be used to get information about what kind of policies
@@ -32,7 +60,7 @@ type Currency struct {
  */
 //TODO Implement endpoint
 //TODO Handle errors
-func getPolicy(w http.ResponseWriter, r *http.Request) {
+func getPolicy(w http.ResponseWriter, r *http.Request) PolicyActions {
 	//Defining variables
 	vars := mux.Vars(r)
 	country := vars["country_name"] //Country name from url
@@ -41,8 +69,13 @@ func getPolicy(w http.ResponseWriter, r *http.Request) {
 	Code := getCountryCode(w, r, country)
 	url := "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/actions/" + Code + "/" + startDate + ""
 	body := invokeGet(w, r, url) //Invoking request
-	fmt.Println(body)
 
+	var policyInfo = PolicyActions{}
+	err := json.Unmarshal([]byte(string(body)), &policyInfo)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return policyInfo
 }
 
 /*
