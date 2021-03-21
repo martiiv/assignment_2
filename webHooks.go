0754012c8c7ca1735a565dfc533a7dd88c736126
Martin Iversen <Martin>
2021-03-21 18:43:37 +0100

    Initialized webHooks.go file for webHooks implementation, other endpoint functionality is finished only error handling remains

---
+++ webHooks.go
@@ -0,0 +1,9 @@
+package main
+
+/*
+ * This file will contain code concerning webhooks implementation
+ * Will interact mainly with database file
+ * author Martin IVersen
+ * version 0.1
+ * date 21.03.2021
+ */
