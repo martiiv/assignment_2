6026ee2daabd493391396ef6d72aa07b7af6fd59
Martin Iversen <Martin>
2021-03-22 20:03:08 +0100

    Created file for firebase database functionality database.go

---
+++ database.go
@@ -0,0 +1,17 @@
+package main
+
+/*
+ * File for firebase(database) functionality
+ * author Martin Iversen
+ * 22.03.2020
+ * version 0.2
+ */
+import (
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"context"
+)
+
+
+var ctx context.Context
+var client *firestore.Client
